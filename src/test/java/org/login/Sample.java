20a562cb92819094db9a8e47605fc53d5311d49e
Sarath13-dev <ramsssjrs@gmail.com>
2025-03-06 14:53:34 +0530

    first

---
+++ src/test/java/org/login/Sample.java
@@ -0,0 +1,44 @@
+package org.login;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+	
+	public static void main(String[] args)  {
+		String s = "dadd is and dadd and is";
+		Map<Character, Integer> m = new LinkedHashMap();
+		for(int i=0; i<s.length(); i++){
+		char x = s.charAt(i);
+		if(m.containsKey(x)){
+			Integer y = m.get(x);
+			m.put(x, y+1);
+		}
+		else{
+			m.put(x, 1);
+		}
+		}
+		
+		System.out.println(m);
+
+	}
+
+}
